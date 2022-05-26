affcdc0966fa5ea249a8b07dcfa6141d262568b3
AkagawaTsurunaki <965145619@qq.com>
2022-05-26 23:25:34 +0800

    MODULE 3: Just a beginning...

---
+++ src/com/github/akagawatsurunaki/roujinfamily/view/HouseKeeperManagementFrame.java
@@ -0,0 +1,47 @@
+package com.github.akagawatsurunaki.roujinfamily.view;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.JScrollBar;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class HouseKeeperManagementFrame extends Frame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTable table;
+	
+	public JTable getTable() {
+		return table;
+	}
+
+	public HouseKeeperManagementFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 529, 412);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		table = new JTable();
+		table.setBounds(10, 56, 478, 309);
+		contentPane.add(table);
+		
+		JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setBounds(488, 56, 17, 309);
+		contentPane.add(scrollBar);
+		
+		JButton btnNewButton = new JButton("修改个人信息");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(10, 10, 115, 37);
+		contentPane.add(btnNewButton);
+	}
+
+}
